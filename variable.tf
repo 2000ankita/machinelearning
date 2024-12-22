46f1589be0546c925a3c08808581295318ba8243
Ankita Vakil <ankitasvakil@gmail.com>
2024-12-22 19:08:56 +0530

    doing another push8

--- variable.tf
+++ variable.tf
@@ -2,14 +2,10 @@
 variable "project_id" {
   description = "The GCP Project ID"
   type        = string
-  #default     = "brigadeomega"
+  default     = "brigadeomega"
 }
 
-variable "tf-service-account" {
-    description = "service account tfcb"
-    type = string
-    #default = "tf-service-account"
-}
+
 # In variables.tf
 variable "GOOGLE_CREDENTIALS" {
   description = "The GCP service account JSON key"
