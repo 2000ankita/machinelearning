c485cc541b9b04c072c5b0961e305359283b0c74
Ankita Vakil <ankitasvakil@gmail.com>
2024-12-22 19:24:12 +0530

    doing another push12

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+project_id      = "brigadeomega"
+account_id      = "service-account-id"
+display_name    = "Service Account"
+GOOGLE_CREDENTIALS = "C:\\Service account\\brigadeomega-cee1c7f667d3.json"  # The GCP service account key JSON file path
--- variable.tf
+++ variable.tf
@@ -1,27 +1,21 @@
-# In variables.tf
-# variable "project_id" {
-#   description = "The GCP Project ID"
-#   type        = string
-#   default     = "brigadeomega"
-# }
+variable "project_id" {
+  description = "The GCP Project ID"
+  type        = string
+}
 
+variable "account_id" {
+  description = "Unique ID for the service account"
+  type        = string
+}
 
-# In variables.tf
+variable "display_name" {
+  description = "Human-readable display name for the service account"
+  type        = string
+}
+
 variable "GOOGLE_CREDENTIALS" {
   description = "The GCP service account JSON key"
   type        = string
   sensitive   = true
 }
 
-# variable "account_id" {
-#   description = "The unique ID for the service account"
-#   type        = string
-#   default     = "tf-service-account"
-# }
-
-# # Optionally, add a display name variable (if it's different)
-# variable "display_name" {
-#   description = "The display name for the service account"
-#   type        = string
-#   default     = "tf-service-account-display" # Optional default
-# }
