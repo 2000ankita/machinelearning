b65e7c1d359042c9f5e5fba14bc550ae7a7ccd15
Ankita Vakil <ankitasvakil@gmail.com>
2024-12-22 18:52:02 +0530

    doing another push5

--- variable.tf
+++ variable.tf
@@ -2,7 +2,7 @@
 variable "project_id" {
   description = "The GCP Project ID"
   type        = string
-  default     ="brigadeomega"
+  default     = "brigadeomega"
 }
 
 variable "tf-service-account" {
